19d7ab3d7628fcefda454ffb13889953e9ba29ec
Magnus Dalsgaard Larsen <daml@itu.dk>
2021-11-06 23:01:04 +0100

    Setup logging and more protobuf

--- server/morris_grpc.go
+++ server/morris_grpc.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"net"
 
 	"github.com/BlueMango10/Nine-men-s-morris/morris"
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -13,9 +16,33 @@ type MorrisServer struct {
 	// Data goes here
 }
 
+func newMorrisServer() *MorrisServer {
+	return &MorrisServer{}
+}
+
+func startServer(address string) {
+	logI(fmt.Sprintf("STARTING SERVER WITH ADDRESS: %s", address))
+	lis, err := net.Listen("tcp", address)
+	if err != nil {
+		logF(err.Error())
+	}
+	var opts []grpc.ServerOption
+	// Server options here (none yet)
+
+	grpcServer := grpc.NewServer(opts...)
+	morris.RegisterMorrisServer(grpcServer, newMorrisServer())
+	err = grpcServer.Serve(lis)
+
+	if err != nil {
+		logF(err.Error())
+	}
+	logI("SERVER STARTED")
+}
+
 func (s *MorrisServer) GetBoardStream(*morris.Empty, morris.Morris_GetBoardStreamServer) error {
 	return status.Errorf(codes.Unimplemented, "method GetBoardStream not implemented")
 }
+
 func (s *MorrisServer) MakeMove(context.Context, *morris.Move) (*morris.BoardState, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method MakeMove not implemented")
 }
